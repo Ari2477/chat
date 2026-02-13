34e1f5c60bc01723e3a831a03b87bbde073e7c8c
Ari <arigato2477@gmail.com>
2026-02-13 20:08:30 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,11 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA4jlTUo2XlfL2-k1gzT3x2tnpqeEZIeUA",
+  authDomain: "mini-messenger-000.firebaseapp.com",
+  projectId: "mini-messenger-000",
+  storageBucket: "mini-messenger-000.firebasestorage.app",
+  messagingSenderId: "456773666595",
+  appId: "1:456773666595:web:59e577a6bbd2240ed6ee96"
+};
 
 const IMGBB_API_KEY = "87b58d438e0cbe5226c1df0a8071621e"; 
 
