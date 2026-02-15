6fdba8b909b0a59909c86e6d6800be6595006c69
Ari <arigato2477@gmail.com>
2026-02-15 23:22:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -746,7 +746,7 @@ function listenForIncomingCalls() {
 
 function playRingtone() {
     stopRingtone();
-    callRingtone = new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
+    callRingtone = new Audio('./sounds/messenger_call.mp3');
     callRingtone.loop = true;
     callRingtone.play().catch(e => console.log('Ringtone play failed:', e));
 }
